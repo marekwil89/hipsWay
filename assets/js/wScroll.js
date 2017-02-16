f24a1ddbf264d6d2c496e4a8d303c5e329f43bac
marekwil89 <marekwil89@gmail.com>
2017-02-16 09:24:31 +0100

    Update wScroll.js

--- assets/js/wScroll.js
+++ assets/js/wScroll.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
-    scrollShowItems(wScroll, '.projects', '.project')
     scrollShowItems(wScroll, '.icons', '.icon-box')
-    scrollMoveItem(wScroll, '.promo', '.flare')
     scrollRotateItem(wScroll, '.promo', '.flare2')
   })
-
-  function scrollMoveItem(wScroll, section , objectToMove){
-    if(wScroll > $(section).offset().top - ($(window).height())) {
-      wScroll = wScroll - $(section).offset().top
-
-      $(objectToMove).css({
-        'transform': 'translateX('+ wScroll +'px)'
-      });
-    }
-  }
 
   function scrollRotateItem(wScroll, section , objectToRotate){
     if(wScroll > $(section).offset().top - ($(window).height())) {
